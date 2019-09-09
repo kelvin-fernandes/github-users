83e8227c1996b265ed5c69c55f5c66cc1e770379
kelvin-fernandes <sokelvinfernandes@gmail.com>
2019-09-08 23:46:30 -0400

    Added form in Main page requesting to github api

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,10 +1,59 @@
-import React from 'react';
-import { View } from 'react-native';
+import React, { Component } from 'react';
+import { Keyboard } from 'react-native';
+import Icon from 'react-native-vector-icons/MaterialIcons';
 
-// import { Container } from './styles';
+import api from '../../services/api';
 
-export default function Main() {
-    return <View />;
+import { Container, Form, Input, SubmitButton } from './styles';
+
+export default class Main extends Component {
+    state = {
+        newUser: '',
+        users: []
+    };
+
+    handleAddUser = async () => {
+        const { newUser, users } = this.state;
+
+        const response = await api.get(`/users/${newUser}`);
+
+        const data = {
+            name: response.data.name,
+            login: response.data.login,
+            bio: response.data.bio,
+            avatar: response.data.avatar_url
+        };
+
+        this.setState({
+            users: [...users, data],
+            newUser: ''
+        });
+
+        Keyboard.dismiss();
+    };
+
+    render() {
+        const { newUser, users } = this.state;
+
+        return (
+            <Container>
+                <Form>
+                    <Input
+                        autoCorrect={false}
+                        autoCapitalize="none"
+                        placeholder="Type a username"
+                        onChangeText={text => this.setState({ newUser: text })}
+                        value={newUser}
+                        returnKeyType="send"
+                        onSubmitEditing={this.handleAddUser}
+                    />
+                    <SubmitButton onPress={this.handleAddUser}>
+                        <Icon name="add" size={20} color="#FFF" />
+                    </SubmitButton>
+                </Form>
+            </Container>
+        );
+    }
 }
 
 Main.navigationOptions = {
