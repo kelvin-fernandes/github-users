c7c084839e3be42cb10afd3dc6bee5ae9abeb9de
kelvin-fernandes <sokelvinfernandes@gmail.com>
2019-09-08 23:00:54 -0400

    Updated main color

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import Routes from './routes';
 export default () => {
     return (
         <>
-            <StatusBar barStyle="light-content" backgroundColor="#7159c1" />
+            <StatusBar barStyle="light-content" backgroundColor="#273c75" />
             <Routes />
         </>
     );
--- src/routes.js
+++ src/routes.js
@@ -15,7 +15,7 @@ const Routes = createAppContainer(
             headerBackTitleVisible: false,
             defaultNavigationOptions: {
                 headerStyle: {
-                    backgroundColor: '#7159c1'
+                    backgroundColor: '#273c75'
                 },
                 headerTintColor: '#FFF'
             }
